5b3e5a399ac2c9e00b17df8869ffe64a0ed3972d
spritly <britaleitner@gmx.de>
2012-08-23 11:01:36 +0200

    globale def datei und neue icons

---
+++ sourcecode/PovRayGraphicCreator/globalDefs.h
@@ -0,0 +1,38 @@
+/* 
+ * File:   globalDefs.h
+ * Author: brita
+ *
+ * Created on 20. August 2012, 19:09
+ */
+
+#ifndef GLOBALDEFS_H
+#define	GLOBALDEFS_H
+
+// Anzeigename in Treeview
+#define NONAME_STR		"undef"
+#define SPHERE_STR		"Kugel"
+#define BOX_STR			"Box"
+#define CYLINDER_STR	"Cylinder"
+#define LIGHT_STR		"Light"
+#define CAMERA_STR		"Camera"
+
+// Icon in Treeview
+#define NO_ICON			"noIcon.jpg"
+#define SPHERE_IMG		"sphereIcon.jpg"
+#define BOX_IMG			"boxIcon.jpg"
+#define CYLINDER_IMG	"cylinderIcon.jpg"
+#define LIGHT_IMG		"lightIcon.jpg"
+#define CAMERA_IMG		"cameraIcon.jpg"
+
+// Identnummer, zb im Treeview zur Obj erzeugung
+#define NO_ID			-10
+#define SPHERE_ID		10
+#define BOX_ID			20
+#define CYLINDER_ID		30
+#define LIGHT_ID		40
+#define CAMERA_ID		50
+
+
+
+#endif	/* GLOBALDEFS_H */
+
